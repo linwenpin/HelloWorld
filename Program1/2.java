4a9d3d971cf662b498f56f0f643068bbb428ca51
lin@123.com <1214427324@qq.com>
2018-10-24 17:58:05 +0800

    change : edit on master

--- Program1/2.java
+++ Program1/2.java
@@ -2,6 +2,7 @@ public class hello
 {
     public static void main(String[] args)
     {
+        //edit on master branch.
         System.out.println("Hello World!"); 
         //edit on dev branch.
     }
