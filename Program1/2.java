525dabcc0df999f91b7ddae63abbdf2ded5a065d
lin@123.com <1214427324@qq.com>
2018-10-24 20:14:39 +0800

    change : edit 2.java by git-test

--- Program1/2.java
+++ Program1/2.java
@@ -2,6 +2,7 @@ public class hello
 {
     public static void main(String[] args)
     {
+        System.out.println("Change From Git Index 'test'"); 
         //edit on master branch.
         System.out.println("Hello World!"); 
         //edit on dev branch.
